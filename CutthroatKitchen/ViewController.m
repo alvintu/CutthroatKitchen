bf7e4dff5f3d607ec75b363a0ec20d75df96564d
hellotojello <tu.alvinj@gmail.com>
2016-10-08 23:25:59 -0400

    create base game properties and small intro

--- CutthroatKitchen/ViewController.m
+++ CutthroatKitchen/ViewController.m
@@ -6,9 +6,18 @@
 //  Copyright © 2016 alvorithms. All rights reserved.
 //
 
+//a single player with AI text-based game based on cutthroat kitchen
+
+
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic) double cookingPower;
+@property (nonatomic) double moralePower;
+@property (nonatomic) double chanceToWin;
+
+
 
 @end
 
@@ -16,12 +25,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)startGame{
+    
+    _cookingPower = 0.0;
+    _moralePower = 0.0;
+    _chanceToWin = 0.0;
+    
+    NSString *intro = @"welcome to cutthroat kitchen.";
+    NSLog(@"%@",intro);
+    [self startShopping];
+    
+    [self presentSabotages];
+    [self startBidding];
+    
+}
+
+-(void)startBidding{
+    
+}
+
+-(void)presentSabotages{
+    
+}
+
+-(void)startShopping{
+    
+}
+
+
+
+-(void)test:(NSString*)test{
+    
+    
 }
 
 @end
